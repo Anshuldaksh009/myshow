57b23bf3306e617d86b96ac200ee091860c66cc7
Anshuldaksh009 <ansulparjapati3@gmail.com>
2026-08-14 17:10:13 +0530

    merge adminmiddleware connection

--- middlewares/adminMiddleware.js
+++ middlewares/adminMiddleware.js
@@ -1,22 +1,21 @@
-
-// this middleware to check the user is admin or client
-
 module.exports = (req, res, next) => {
     try {
-        console.log(req.body);
-        
-      //  req.body.userRole was safely attached by your authMiddleware earlier!
-        if (req.body.userRole !== 'admin') {
+        console.log("User:", req.user);
+        console.log("User Role:", req.userRole);
+
+        if (req.userRole !== 'admin') {
             return res.status(403).send({
                 success: false,
                 message: "Access Denied. Only system Administrators can perform this action."
             });
         }
 
-        //If they are an admin, let them pass to the controller
         next();
 
     } catch (error) {
-        res.status(500).send({ success: false, message: error.message +"problem in admin middleware" });
+        res.status(500).send({
+            success: false,
+            message: error.message + " problem in admin middleware"
+        });
     }
 };
