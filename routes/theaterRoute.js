7ce4ec90ef8f776500e3a8035ee83cb6845037f6
Anshuldaksh009 <ansulparjapati3@gmail.com>
2026-08-14 16:49:10 +0530

    Added middleware in theatereroute

--- routes/theaterRoute.js
+++ routes/theaterRoute.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const { addTheater, getAllTheaters } = require('../controllers/theaterController');
 
+//for check the token 
+const authMiddleware=require('../middlewares/authMiddleware.js')
+const  adminMiddleware=require('../middlewares/adminMiddleware.js')
 router.post('/add', authMiddleware, adminMiddleware, addTheater);
 router.get('/get-all', getAllTheaters);
 
